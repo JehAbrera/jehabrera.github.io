3a699e066fee39cab5a2e7c91b9f50356d943c55
JehAbrera <95663191+JehAbrera@users.noreply.github.com>
2024-03-24 01:19:41 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,php,js}"],
+  theme: {
+    extend: {
+      flex: {
+        '2': '2 2 0%'
+      }
+    },
+  },
+  plugins: [],
+}
+
